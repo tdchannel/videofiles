35552046c76df1a70f419815b76cfe0bde842aca
Rudy Cortes <fxjeane@gmail.com>
2012-06-20 15:33:00 -0700

    removed waitForReads()/confirmation out of network transfer

--- rendering/pytracer/driver/framebuffer.py
+++ rendering/pytracer/driver/framebuffer.py
@@ -30,49 +30,47 @@ class TcpServer(QTcpServer):
         self.socket = self.nextPendingConnection()
         self.socket.parent = self.parent
         self.sockets[self.socket.socketDescriptor()] = self.socket
-        self.socket.write(QByteArray("1"))
+        #self.socket.write(QByteArray("1"))
         QObject.connect( self.socket, SIGNAL("readyRead()"), self.tcpSocketReadyReadEmitted )
-
+        self.socket.readyRead.emit()
+        
     def tcpSocketReadyReadEmitted(self):
         txt = str(self.socket.readAll())
-        
-        txtAr = struct.unpack("6s",txt[:6])
-        #print txtAr
-        #print txt
-        #txtAr = txt.split()
-       
-        if txtAr[0] == "imgnfo":
-            uvals = struct.unpack("II",txt[8:])
-            # resize the window
-            w = int(uvals[0])
-            h = int(uvals[1])
-            self.parent.resize(w,h)
-            px = QtGui.QPixmap(w,h)
-            px.fill(Qt.black)
-            lbl = QtGui.QLabel()
-            lbl.setPixmap(px)
-            self.parent.layout().addWidget(lbl)
+        while len(txt) > 0:
+            txtAr = struct.unpack("6s",txt[:6])
+            txt = txt[8:]  
+            if txtAr[0] == "imgnfo":
+                uvals = struct.unpack("II",txt[:8])
+                txt = txt[8:]
+                # resize the window
+                w = int(uvals[0])
+                h = int(uvals[1])
+                self.parent.resize(w,h)
+                px = QtGui.QPixmap(w,h)
+                px.fill(Qt.black)
+                lbl = QtGui.QLabel()
+                lbl.setPixmap(px)
+                self.parent.layout().addWidget(lbl)
+                
+            elif txtAr[0] == "bucket":
+                uvals = struct.unpack("IIII",txt[:16])
+                txt = txt[16:]
+                upack = 3*uvals[2]*uvals[3]
+                pixels = struct.unpack("%dI"%upack,txt[:upack*4])
+                txt = txt[upack*4:]
+                lbl = self.parent.layout().currentWidget()
+                px = lbl.pixmap()
+                img = px.toImage()
 
-        elif txtAr[0] == "bucket":
-            uvals = struct.unpack("IIII",txt[8:24])
-            pixels = struct.unpack("%dI"%3*uvals[2]*uvals[3],txt[24:])
-            #print pixels
-            lbl = self.parent.layout().currentWidget()
-            px = lbl.pixmap()
-            img = px.toImage()
-
-            for y in range(uvals[3]):
-                for x in range(uvals[2]):
-                    tpix = ((y * uvals[2]) + x) * 3
-                    value = QtGui.qRgb(pixels[tpix], 
-                                       pixels[tpix+1],
-                                       pixels[tpix+2])
-                    #print pixels[tpix],pixels[tpix+1],pixels[tpix+2]
-                    img.setPixel(uvals[0]+x,uvals[1]+y,value)
-            px = px.fromImage(img)
-            lbl.setPixmap(px)
-
-        self.socket.write(QByteArray("1"))
+                for y in range(uvals[3]):
+                    for x in range(uvals[2]):
+                        tpix = ((y * uvals[2]) + x) * 3 
+                        value = QtGui.qRgb(pixels[tpix], 
+                                           pixels[tpix+1],
+                                           pixels[tpix+2])
+                        img.setPixel(uvals[0]+x,uvals[1]+y,value)
+                px = px.fromImage(img)
+                lbl.setPixmap(px)
 
 if __name__ == '__main__':
     a = QtGui.QApplication(sys.argv)
